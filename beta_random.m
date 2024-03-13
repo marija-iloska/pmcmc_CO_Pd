c0d671451ffa36e082df6d9910da3462563de4b9
marija-iloska <50119390+marija-iloska@users.noreply.github.com>
2024-03-13 19:53:58 -0400

    sample from beta with specified mean

--- beta_random.m
+++ beta_random.m
@@ -1,7 +1,9 @@
 function [Y] = beta_random(alpha, mu)
 
+% Compute beta parameter based on define alpha and expected mean
 beta  = (1-mu).*alpha./mu;
 
+% Sample random variable
 Y = betarnd(alpha, beta);
 
 
