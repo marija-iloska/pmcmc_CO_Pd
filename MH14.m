f8f179428d24aa644768a0c9909302712935acea
marija-iloska <50119390+marija-iloska@users.noreply.github.com>
2024-03-13 19:24:17 -0400

    metropolis hastings sampling steps

--- MH14.m
+++ MH14.m
@@ -1,12 +1,11 @@
-function [x] = MH14op(y, regions, x_old, bounds, P, dt, var, alpha4, alpha, alpha1, beta1)
+function [x] = MH14(y, regions, x_old, bounds, P, dt, alpha4, alpha, alpha1, beta1)
 
 % Restore last values
-[~, cut_off, M, ~] = bounds{:};
+[~, cut_off, ~, M, ~] = bounds{:};
 
 % Old values
 k1_old = x_old(1);
 k4_old = x_old(2);
-x1_old = x_old(3);
 
 % Get old proposal for a4 q(a4 | alpha4, beta4_old)
 beta4_old = (1 - k4_old*dt)*alpha4/(k4_old*dt);
@@ -19,10 +18,7 @@ a4_old = 1 - k4_old*dt;
 % Propose sample for k1
 x1_star = gamrnd(alpha1, beta1);
 k1_star = k4_star*cut_off/(M - cut_off)/P + x1_star;
-
-
 k1_lim = (M - a4_star*cut_off)/(dt*P*(M - cut_off));
-
 k1_star = min([k1_lim, k1_star]);
 
 % Get new porposal parameters
@@ -38,8 +34,6 @@ yR4 = y(regions{4});
 
 
 % Form a params
-
-
 a1_star = k1_star*P*dt;
 a1_old = k1_old*P*dt;
 
@@ -47,12 +41,13 @@ a1_old = k1_old*P*dt;
 x_star = [k1_star, k4_star, x1_star];
 
 
+% REGION IV --------------------------------------------------------
 
-% REGION IV
+% Mean of likelihood
 mu_star =  2*a4_star*yR4(1:end-1);
 mu_old = 2*a4_old*yR4(1:end-1);
 
-% Beta param
+% Beta param old and proposed 
 beta_star = (1-mu_star).*alpha./mu_star;
 beta_old = (1-mu_old).*alpha./mu_old;
 
@@ -64,30 +59,22 @@ ln_l4 = sum( (beta_star - beta_old).*log(1 - 2*yR4(2:end)));
 
 
 
-% REGION I
+% REGION I--------------------------------------------------------
+
+% Mean of likelihood
 mu_star =  2*(a1_star*yR1A(1:end-1) + a4_star*yR1(1:end-1));
 mu_old = 2*(a1_old*yR1A(1:end-1) + a4_old*yR1(1:end-1));
 
-% Beta param
+% Beta param old and proposed
 beta_star = (1-mu_star).*alpha./mu_star;
 beta_old = (1-mu_old).*alpha./mu_old;
 
 % Log likelihood ratio
 ln_l1 = sum((beta_star - beta_old).*(log(1 - 2*yR1(2:end))));
 
-% Proposal ratios
-% ln_q1 = log(gamma(x1_star)/gamma(x1_old)) + alpha1_old*log(beta1_old/x1_star) - alpha1_star*log(beta1_star/x1_old) +...
-%     log(x1_star/x1_old) + x1_star/alpha1_old - x1_old/alpha1_star;
-% 
 
-
-% LOG Likelihood ratio
-AR = exp(ln_l1 + ln_l4 + ln_q4); %+ ln_q1);
-
-% Correction
-if (isreal(AR) == 0)
-    AR = 0;
-end
+% ACCEPTANCE RATIO --------------------------------------------------
+AR = exp(ln_l1 + ln_l4 + ln_q4);
 
 % Accept or Reject
 if (rand < AR)
